06e78aedfb5fde704228e177bc150f7eca3304c2
Abdul Kavi <165765669+abkavi9924@users.noreply.github.com>
2026-02-06 16:19:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -65,10 +65,8 @@ class RestorationUNet(nn.Module):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-
+            nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
@@ -102,3 +100,4 @@ class PerceptualLoss(nn.Module):
         output_features = self.vgg(output)
         target_features = self.vgg(target)
         return nn.MSELoss()(output_features, target_features)
+
